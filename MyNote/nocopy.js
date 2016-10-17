4f564cb79d1b7f18e8ce037b33c483888f4139c5
zhangzhuo <zhangzhuo@orivon.com>
2016-10-17 17:00:14 +0800

    增加note

---
+++ MyNote/nocopy.js
@@ -0,0 +1,12 @@
+document.oncontextmenu = function() {
+    return false;
+}
+document.body.oncopy = function() {
+    return false;
+}
+document.body.onpaste = function() {
+    return false;
+}
+document.body.oncut = function() {
+    return false;
+}
